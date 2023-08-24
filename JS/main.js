354f4410e51f8da2cffbb512b4471d5decbb170f
luyvannda <philips.joe329@gmail.com>
2023-08-24 15:05:14 +0700

    store all components on our html in our JS file

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // store all components on html in our JS
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  let clear = document.querySelector(".clear");
+  let back = document.querySelector(".back");
+
+  let operators = document.querySelectorAll(".operator");
+  let numbers = document.querySelectorAll(".number");
+
+  let decimal = document.querySelector(".decimal");
+  let equal = document.querySelector(".equal");
+})
