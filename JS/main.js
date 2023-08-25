55acdc87ec30e7b1afd8fb81c708b51e5ff0a3cb
luyvannda <philips.joe329@gmail.com>
2023-08-25 09:30:37 +0700

    add function for clear button successfully, still need to find ways to deal with back button, though.

--- JS/main.js
+++ JS/main.js
@@ -28,17 +28,43 @@ document.addEventListener("DOMContentLoaded", function () {
       handleOperator(e.target.textContent);
       previousScreen.textContent = previousValue + ' ' + operator;
       currentScreen.textContent = currentValue;
-    }))
+    }));
+
+  clear.addEventListener("click", function () {
+    previousValue = '';
+    currentValue = '';
+    operator = '';
+    previousScreen.textContent = previousValue;
+    currentScreen.textContent = currentValue;
+  });
+
+
+  back.addEventListener("click", function () {
+    handleBack();
+    currentScreen.textContent = currentValue;
+  })
 });
+
+// outside function starts here
 
 function handleNumber(num) {
   if (currentValue.length <= 10) {
     currentValue += num;
   }
-}
+};
 
 function handleOperator(op) {
   operator = op;
   previousValue = currentValue;
   currentValue = '';
-}
+};
+
+function handleBack() {
+  if (currentValue.length > 0) {
+    currentValue = currentValue
+      .substring(0, currentValue.length - 1);
+  } else if (operator.length > 0) {
+    operator = '';
+    previousValue = '';
+  }
+};
